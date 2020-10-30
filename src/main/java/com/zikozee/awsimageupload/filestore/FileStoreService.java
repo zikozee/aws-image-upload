6f13bb472f613989bfa49976ddfb047d864d7766
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-10-30 16:32:38 +0100

    Saving files to Bucket. closes #1

---
+++ src/main/java/com/zikozee/awsimageupload/filestore/FileStoreService.java
@@ -0,0 +1,10 @@
+package com.zikozee.awsimageupload.filestore;
+
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Optional;
+
+public interface FileStoreService {
+
+    void save(String path, String fileName, Optional<Map<String, String>> optionalMetadata, InputStream inputStream);
+}
