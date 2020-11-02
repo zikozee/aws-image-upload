7a0e4819b1cc6f967bd7e7417cf0ee0fb816c5cf
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-11-02 19:45:56 +0100

    Upload Image API. closes #5

--- src/main/java/com/zikozee/awsimageupload/profile/UserProfileDataAccessService.java
+++ src/main/java/com/zikozee/awsimageupload/profile/UserProfileDataAccessService.java
@@ -3,8 +3,10 @@ package com.zikozee.awsimageupload.profile;
 import com.zikozee.awsimageupload.datastore.FakeUserProfileDataStore;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -16,5 +18,10 @@ public class UserProfileDataAccessService implements UserProfileService{
     @Override
     public List<UserProfile> getUserProfiles(){
         return fakeUserProfileDataStore.getUserProfiles();
+    }
+
+    @Override
+    public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
+
     }
 }
--- src/main/java/com/zikozee/awsimageupload/profile/UserProfileService.java
+++ src/main/java/com/zikozee/awsimageupload/profile/UserProfileService.java
@@ -2,11 +2,15 @@ package com.zikozee.awsimageupload.profile;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 
 public interface UserProfileService {
 
     List<UserProfile> getUserProfiles();
+
+    void uploadUserProfileImage(UUID userProfileId, MultipartFile file);
 }
